88e9298f0a4c3ac9440b68faed537f3cfb4a26b0
vijaymhr <vijaymaharjan911@gmail.com>
2019-11-13 15:15:00 +1300

    home

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -13,7 +13,7 @@
 
 	      <div class="collapse navbar-collapse" id="ftco-nav">
 	        <ul class="navbar-nav ml-auto">
-	        	<li class="nav-item active"><a href="/" class="nav-link">HOME</a></li>
+	        	<li class="nav-item active"><a href="/" class="nav-link">Home</a></li>
 	        	<li class="nav-item"><a href="gallery" class="nav-link">Gallery</a></li>
 	        	<li class="nav-item"><a href="about" class="nav-link">About Us</a></li>
 	        	<li class="nav-item"><a href="contact" class="nav-link">Contact</a></li>

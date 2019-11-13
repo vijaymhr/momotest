415f031bf9df76b7a9626b7fb5cdd5e5eb923bb6
AndriiKry <andrii.krysko@gmail.com>
2019-11-14 12:10:51 +1300

    andrii

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -122,73 +122,8 @@
   </section>
 
 
-  
-  <section class="ftco-section img" style="background-image: url(images/bg_3.jpg)" data-stellar-background-ratio="0.5">
-    <div class="container">
-      <div class="row d-flex">
-        <div class="col-md-7 ftco-animate makereservation p-4 px-md-5 pb-md-5">
-          <div class="heading-section ftco-animate mb-5 text-center">
-            <span class="subheading">Book a table</span>
-            <h2 class="mb-4">Make Reservation</h2>
-          </div>
-          <form action="#">
-            <div class="row">
-              <div class="col-md-6">
-                <div class="form-group">
-                  <label for="">Name</label>
-                  <input type="text" class="form-control" placeholder="Your Name">
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="form-group">
-                  <label for="">Email</label>
-                  <input type="text" class="form-control" placeholder="Your Email">
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="form-group">
-                  <label for="">Phone</label>
-                  <input type="text" class="form-control" placeholder="Phone">
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="form-group">
-                  <label for="">Phone</label>
-                  <input type="text" class="form-control" id="book_date" placeholder="Date">
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="form-group">
-                  <label for="">Time</label>
-                  <input type="text" class="form-control" id="book_time" placeholder="Time">
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="form-group">
-                  <label for="">Person</label>
-                  <div class="select-wrap one-third">
-                    <div class="icon"><span class="ion-ios-arrow-down"></span></div>
-                    <select name="" id="" class="form-control">
-                      <option value="">Person</option>
-                      <option value="">1</option>
-                      <option value="">2</option>
-                      <option value="">3</option>
-                      <option value="">4+</option>
-                    </select>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-12 mt-3">
-                <div class="form-group text-center">
-                  <input type="submit" value="Make a Reservation" class="btn btn-primary py-3 px-5">
-                </div>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  </section>
+  <hr>
+
   
   <section class="ftco-section ftco-wrap-about">
     <div class="container">
